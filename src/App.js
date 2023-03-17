dc026e6c4ad40cbbdf52df61aef1f6bcf5e0f785
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-17 20:09:30 +0300

    <

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-
+import Sectiontwo from "./Components/Sectiontwo";
+import React from 'react'
 
 function App() {
   return (
     // Header Section
     <>
-    <header className="bg-black py-3 lg:py-6 text-gray-400 font-serif uppercase">
+    <header className="bg-black py-3 pl-10 lg:py-6 text-gray-400 font-serif uppercase">
       {/* Header Container */}
     <div className="container flex items-center justify-between space-x-8 lg:space-x-16 ">
       {/* LOGO */}
-      <h1 className=" lg:text-6xl text-4x1 font-bold text-gray-400 text-transparent bg-gradient-to-r bg-clip-text from-slate-500 to-slate-200">YEK</h1>
+      <h1 className=" lg:text-4xl text-3x1 font-bold text-gray-400 text-transparent bg-gradient-to-r bg-clip-text from-slate-500 to-slate-200">YEKFLIX</h1>
+     
       {/* Navigation */}
       <nav className="flex justify-between flex-1">
-        <div className="flex py-6 items-start text-xs lg:text-base space-x-8">
+        {/* MENU */}
+        <div className="hidden md:flex py-6 items-start text-xs lg:text-base space-x-4 lg:space-x-8">
           <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">Movies</a>
           <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">Celebrities</a>
           <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">News</a>
           <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">Blog</a>
           <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">About</a>
-          
-        </div>
-        {/* Login Area */}
-        <div className="flex items-center space-x-8">
-        {/* Search Area */}
-        <form>
+          <form>
           <div className="border-r px-4  py-1 border-red-400">
-            <input type="text" className="bg-transparent border-b border-red-500  focus:outline-none" placeholder="Search.."></input>
+            <input type="text" 
+            className="bg-transparent border-b border-red-500 w-24 lg:w-44 focus:outline-none" 
+            placeholder="Search.."></input>
             <button className="font-semibold"><i className="fas fa-search"/></button>
           </div>
 
         </form>
+        </div>
+        {/* Login Area */}
+        <div className="flex items-center space-x-4 lg:space-x-8">
+        {/* Search Area */}
+        
         {/* Signup Area */}
-        <div className="flex items-center space-x-8 text-sm">
-          <a className="cursor">Login</a>
+        <div className="hidden md:text-xs md:flex items-center space-x-4 lg:space-x-8 lg:text-base">
+          <a className="cursor-pointer md:text-sm">Login</a>
           <a className="bg-red-600 px-3 py-1 hover:bg-rose-600 hover:text-gray-600 cursor-pointer transition duration-100 rounded-sm whitespace-nowrap">Sign Up</a>
         </div>
         </div>
       </nav>
+       {/* Mobile Menu */}
+       <div className="block md:hidden mr-14">
+        <div className="space-y-1 cursor-pointer">
+          <div className="bg-slate-500 w-8 h-1 rounded-full"></div>
+          <div className="bg-slate-500 w-8 h-1 rounded-full"></div>
+          <div className="bg-slate-500 w-8 h-1 rounded-full"></div>
+        </div>
+      </div>
     </div>
     
 
 
 
     </header>
+    <Sectiontwo/>
+   
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     container: {
       center : true,
@@ -9,9 +9,14 @@ module.exports = {
         xl:'1140px',
         '2xl':'1140px',
         md :'768px'
+      },
+      
+    },
+    extend: {
+      spacing:{
+        '128':"32rem"
       }
     },
-    extend: {},
   },
   plugins: [],
 }
